bf2d0426b57b468cdd488b86fb2cc684bd025b03
Tabitha Steffens <tabitha.steffens@student.pxl.be>
2024-10-04 12:27:49 +0200

    done

--- snelste-atleet/MainWindow.xaml.cs
+++ snelste-atleet/MainWindow.xaml.cs
@@ -16,14 +16,60 @@ namespace snelste_atleet
     /// </summary>
     public partial class MainWindow : Window
     {
+        string nameFastest;
+        int secondsFastest = 0;
         public MainWindow()
         {
             InitializeComponent();
         }
-
+        
         private void newButton_Click(object sender, RoutedEventArgs e)
+        { 
+            bool isValidNumber = int.TryParse(timeTextBox.Text, out int secondsCurrent);
+
+            if (isValidNumber)
+            {
+                if (secondsFastest == 0 ||secondsCurrent < secondsFastest)
+                {
+                    secondsFastest = secondsCurrent;
+                    nameFastest = nameTextBox.Text;
+                }
+            }
+
+            nameTextBox.Clear();
+            timeTextBox.Clear();
+            nameTextBox.Focus();
+        }
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
         {
+            bool isValidNumber = int.TryParse(timeTextBox.Text, out int secondsCurrent);
+
+            if (isValidNumber)
+            {
+                if (secondsFastest == 0 || secondsCurrent < secondsFastest)
+                {
+                    secondsFastest = secondsCurrent;
+                    nameFastest = nameTextBox.Text;
+                }
+            }
+
+            resultTextBox.Text = $"De snelste atleet is {nameFastest} met de tijd van {secondsFastest}.";
 
         }
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            nameTextBox.Clear();
+            timeTextBox.Clear();
+            resultTextBox.Clear();
+
+            nameTextBox.Focus();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+       
     }
 }
